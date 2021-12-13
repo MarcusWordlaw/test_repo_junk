9c0b65448ca9512ea0ed299d4e4639276c8bd59f
Marcus <user1@mail.com>
2021-12-13 16:41:51 +0000

    netlify

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ export default function App() {
       <BrowserRouter>
         <Routes>
         <Route path="/" element={<SinglePagePDFViewer pdf={samplePDF} />} />
-        <Route path="/ObterProcessokey=TEdNMTMzMzhTTEkqQ0UzUDNFM01HTlNMSSpDRTExLTEyLTIwMjEgMTg6MTA6MTB" element={<SinglePagePDFViewer pdf={smPDFB} />} />
-        <Route path="/ObterProcessokey=TEdNMTMzMzhTTEkqQ0UzUDNFM01HTlNMSSpDRTExLTEyLTIwMjEgMTg6MTA6MTM" element={<SinglePagePDFViewer pdf={smPDFM} />} />
+        <Route path="/TEdNMTMzMzhTTEkqQ0UzUDNFM01HTlNMSSpDRTExLTEyLTIwMjEgMTg6MTA6MTB" element={<SinglePagePDFViewer pdf={smPDFB} />} />
+        <Route path="/TEdNMTMzMzhTTEkqQ0UzUDNFM01HTlNMSSpDRTExLTEyLTIwMjEgMTg6MTA6MTM" element={<SinglePagePDFViewer pdf={smPDFM} />} />
         </Routes>
       </BrowserRouter>
       <hr />
