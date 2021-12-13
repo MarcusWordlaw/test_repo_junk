ad67d92378a385b9dbf858ac600264045b0fba5b
Marcus <user1@mail.com>
2021-12-13 16:07:46 +0000

    new com

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+
+import SinglePagePDFViewer from "./components/pdf/single-page";
+/* This is required only if the project file is located 
+inside the app. Otherwise you can use the external link of the pdf file*/
+import samplePDF from "./sample.pdf";
+import smPDFM from "./smpM.pdf";
+import smPDFB from "./smpB.pdf";
+
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h4>RT-PCR</h4>
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<SinglePagePDFViewer pdf={samplePDF} />} />
+        <Route path="/ObterProcessokey=TEdNMTMzMzhTTEkqQ0UzUDNFM01HTlNMSSpDRTExLTEyLTIwMjEgMTg6MTA6MTB" element={<SinglePagePDFViewer pdf={smPDFB} />} />
+        <Route path="/ObterProcessokey=TEdNMTMzMzhTTEkqQ0UzUDNFM01HTlNMSSpDRTExLTEyLTIwMjEgMTg6MTA6MTM" element={<SinglePagePDFViewer pdf={smPDFM} />} />
+        </Routes>
+      </BrowserRouter>
+      <hr />
+    </div>
+  );
+}
+
+
+// Utentes/Home/ObterProcesso?key=TEdNMTMzMzhTTEkqQ0UzUDNFM01HTlNMSSpDRTExLTEyLTIwMjEgMTg6MTA6MTB
